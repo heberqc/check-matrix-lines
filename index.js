d324f580a3e66c52cdcd081a00d428961aeb6555
heberqc <heberqc@gmail.com>
2020-03-16 17:05:22 -0500

    check matrix horizontally

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 
 function checkVector(vector, value) {
-  // console.log(vector, value)
+  // console.log('checkVector', vector, value)
   return vector.every(el => el === value)
 }
 
@@ -20,7 +20,7 @@ function checkVector(vector, value) {
 // console.log(checkVector([2,2,2,2], 2))
 
 function checkLine(vector, value, minLen) {
-  // console.log(vector, value, minLen)
+  // console.log('checkLine', vector, value, minLen)
   if (vector.length >= minLen) {
     if (checkVector(vector.slice(0, minLen), value)) {
       return true;
@@ -32,9 +32,61 @@ function checkLine(vector, value, minLen) {
   }
 }
 
-console.log('[1,2,3,4]', checkLine([1,2,3,4], 2, 3))
-console.log('[1,2,2,1]', checkLine([1,2,2,1], 2, 3))
-console.log('[1,2,1,1]', checkLine([1,2,1,1], 1, 3))
-console.log('[2,1,1,1]', checkLine([2,1,1,1], 1, 3))
-console.log('[1,2,2,2]', checkLine([1,2,2,2], 2, 3))
-console.log('[1,2,2,2,1]', checkLine([1,2,2,2,1], 2, 3))
+// console.log('[1,2,3,4]', checkLine([1,2,3,4], 2, 3))
+// console.log('[1,2,2,1]', checkLine([1,2,2,1], 2, 3))
+// console.log('[1,2,1,1]', checkLine([1,2,1,1], 1, 3))
+// console.log('[2,1,1,1]', checkLine([2,1,1,1], 1, 3))
+// console.log('[1,2,2,2]', checkLine([1,2,2,2], 2, 3))
+// console.log('[1,2,2,2,1]', checkLine([1,2,2,2,1], 2, 3))
+
+function checkHorizontal(matrix, value, minLen) {
+  // console.log('checkHorizontal', matrix, value, minLen)
+  if (matrix.length === 1) {
+    return checkLine(matrix.slice(0, 1)[0], value, minLen);
+  } else {
+    if (checkLine(matrix.slice(0, 1)[0], value, minLen)) {
+      return true;
+    } else {
+      return checkHorizontal(matrix.slice(1), value, minLen);
+    }
+  }
+}
+
+console.log(`[
+  [0,0,1,0,0],
+  [1,1,0,1,0],
+  [0,1,1,1,0],
+]` ,checkHorizontal([
+  [0,0,1,0,0],
+  [1,1,0,1,0],
+  [0,1,1,1,0],
+], 1, 3));
+console.log(`[
+  [0,0,1,0,0],
+  [1,1,0,1,0],
+  [0,1,0,1,0],
+]` ,checkHorizontal([
+  [0,0,1,0,0],
+  [1,1,0,1,0],
+  [0,1,0,1,0],
+], 1, 3));
+console.log(`[
+  [0,0,1,0,0],
+  [1,1,0,1,0],
+  [0,1,1,1,0],
+]` ,checkHorizontal([
+  [0,0,1,0,0],
+  [1,1,0,1,0],
+  [0,1,1,1,0],
+], 0, 3));
+console.log(`[
+  [0,0,1,0,0],
+  [1,1,0,1,0],
+  [1,1,2,2,2],
+  [0,1,1,1,0],
+]` ,checkHorizontal([
+  [0,0,1,0,0],
+  [1,1,0,1,0],
+  [1,1,2,2,2],
+  [0,1,1,1,0],
+], 2, 3));
