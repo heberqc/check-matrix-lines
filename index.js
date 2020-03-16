125e32c732d63b8278bba73148cdda95e764d515
heberqc <heberqc@gmail.com>
2020-03-16 14:48:49 -0500

    check vector of values

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+
+function checkVector(vector, value) {
+  // console.log(vector, value)
+  return vector.every(el => el === value)
+}
+
+console.log(checkVector([1,2,3,4], 2))
+console.log(checkVector([1,2,2,1], 2))
+console.log(checkVector([1,2,1,1], 1))
+console.log(checkVector([1,1,1,1], 1))
+console.log(checkVector([1,2,1,1], 1))
+console.log(checkVector([1,1,1], 2))
+console.log(checkVector([2,2,2,2], 2))
